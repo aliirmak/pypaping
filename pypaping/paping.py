485c4515fb5c6fd8a48c7bce27495d59fe93c2f2
Ali Irmak Ozdagli <aliirmak@gmail.com>
2013-04-11 18:50:12 -0400

    A new version 1.3.0
    Implemented email option. Doesn't work superior, as i wanted to but
    still getting the job done!
    Hopefully, this will be not the motto of this program.

--- pypaping/paping.py
+++ pypaping/paping.py
@@ -7,6 +7,8 @@ It essentially pings the host and checks the RTT
 and if there is time out error.
 All of the data is written later to a .csv file wrt 
 to the time when ping test conducted.
+If a certain amount of time out errors are achieved then
+program emails to the user.
 The software uses paping.exe to accomplish the task.
 Currently, paping can only ping TCP ports.
 WireShark reports the typical TCP packet size is 66 bytes or 528 bits.
@@ -17,8 +19,6 @@ Personally suggesting to get x86 version
 
 Next tasks for far far future
 1) give error for large time delays
-2) email to the user if any exception is happening during the run-time such as
-   long time delays or repeated time outs
 """
 
 """
@@ -27,16 +27,18 @@ Rev hist:
 1.2: It writes the max and min ping values to the file now
      It creates a new output.txt file each time program runs
 1.2.1: GitHUb rep on https://github.com/aliirmak/pypaping/
+1.3.0: PyPaping emails when there is too much of time out error
 """
 
 ## written by Ali Irmak Ozdagli, 2013
-## Version: 1.2.1
+## Version: 1.3.0
 
-import os
-import re
-import csv
-import datetime
-import time
+import os       # for commands through dos/cmd
+import re       # for regex functions to search for specific strings
+import csv      # for writing csv file
+import datetime # for recording time
+import time     # for timer
+import smtplib  # for sending email within this application
 
 # writes the results to a csv file
 # each row contains time and min/max/avg ms
@@ -45,7 +47,10 @@ def write_to_file(date_file_name, n_t_argument):
         with open('output_' + date_file_name + '.txt', 'a', newline='') as outputfile:
             wrtr  = csv.writer(outputfile, dialect = 'excel-tab')
             now_wrt = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-            text_input = [now_wrt] + n_t_argument
+            if len(n_t_argument) != 3:   # checks here if three arguments are passed or not
+                text_input = [now_wrt, n_t_argument]
+            else:
+                text_input = [now_wrt] + n_t_argument
             wrtr.writerow(text_input)
     finally:
         outputfile.close()
@@ -57,6 +62,19 @@ def check_time(until_when):
     if until_when < datetime.datetime.now():
         input('Task has been finished.\nPress any key to quit\n')
         exit()    
+
+# sends email when a lot of time out errors are created
+# This part takes some time to process, so sometimes wait_time may be larger
+# than the timer_tick
+def send_email_alert(fromaddr, toaddrs, msg, username, password, sptm_server):
+    server = smtplib.SMTP(sptm_server)
+    server.ehlo()  
+    server.starttls()  
+    server.ehlo()
+    server.login(username,password)  
+    server.sendmail(fromaddr, toaddrs, msg)  
+    server.quit() 
+    
 
 # First check if the paping file exist
 # if it does not program quits
@@ -77,17 +95,29 @@ then = now + datetime.timedelta(**time_length)
 date_file = datetime.datetime.now().strftime('%Y%m%d%H%M%S') # will be used to open the text file
 
 # how frequently do you want to run the test?
-timeout = 60
+timer_tick = 60
 
 # default paping interface parameters
 my_command = "paping"
-myUrl = "128.46.160.229"
+myUrl = "xxx.xxx.xxx.xxx"  # this should be an ip address you want to ping
 myport = "-p 80"
 my_count = "-c 4"
 my_tout = "-t 1000"
 nocolor = "--nocolor"
 paping_command = my_command + ' ' + myUrl + ' ' + myport + ' ' + my_count + ' ' + my_tout + ' ' + nocolor
 
+# parameters regarding email service
+counter_to = 0 # initialization of variable that counts how many time outs are there
+limit_to = 4   # a time out lime; when achieved email will be sent. should be larger than 0
+fromaddr = 'username@gmail.com'  
+toaddrs  = ['username@gmail.com']  
+msg_subject = 'PyPaping Time out Error'
+msg_body = 'PyPaping is getting many time outs! Server may be down.' 
+msg = 'Subject: %s\n\n%s' % (msg_subject, msg_body)   
+username = 'username'  
+password = 'password'  
+sptm_server = 'smtp.gmail.com:587'
+mail_arg = [fromaddr, toaddrs, msg, username, password, sptm_server]
 
 while True:
     # opens the command window and sends the ping command
@@ -114,14 +144,20 @@ while True:
     if toe:
         error_to = 'Time out error!'
         print(error_to)
-        write_to_file(error_to)
+        write_to_file(date_file, error_to)
+        counter_to += 1
+        if counter_to >= limit_to :
+            send_email_alert(*mail_arg)
+            counter_to = 0
     else:
         print('Average latency is ' + str(avr_ping) +' ms')
         write_to_file(date_file, [min_ping, max_ping, avr_ping] )
+        counter_to = 0
+        
     
     # check the time. if current time is larger then the time set, program will stop    
     check_time(then)
     
-    # Pause program for the next iteration for timeout value. 
+    # Pause program for the next iteration for timer_tick value. 
     # When time passes next while loop will start 
-    time.sleep(timeout)    
+    time.sleep(timer_tick)    
--- pypaping/plotter.m
+++ pypaping/plotter.m
@@ -6,12 +6,16 @@ clc
 read_file = tdfread('output.txt');
 fields_file =  fieldnames(read_file);
 time = read_file.(fields_file{1,1});
-avg_ping = read_file.(fields_file{2,1});
+min_ping = read_file.(fields_file{2,1});
+max_ping = read_file.(fields_file{3,1});
+avg_ping = read_file.(fields_file{4,1});
 
 %% assign outputs to vars
 % assign avg_pin_numbers to avg_ping_no
 % assign time to xdate
 for i = 1:length(avg_ping)
+    min_ping_no(i,1) = str2double(min_ping(i,:));
+    max_ping_no(i,1) = str2double(max_ping(i,:));
     avg_ping_no(i,1) = str2double(avg_ping(i,:));
 end
 
@@ -22,14 +26,14 @@ end
 xdate = datenum(time,'yyyy/mm/dd HH:MM:SS');
 
 %% plot the values
-plot(xdate,avg_ping_no,...
+plot(xdate,[min_ping_no, max_ping_no, avg_ping_no],...
     'MarkerFaceColor',[0 0 0],'MarkerEdgeColor',[1 0 0],...
     'MarkerSize',2,...
     'Marker','o',...
     'LineWidth',2)
 datetick('x','HHPM')
 box on; grid on;
-legend('Average Ping')
+legend('Min Ping','Max Ping','Average Ping')
 xlabel('Time')
 ylabel('Pings in ms')
 title(['Ping numbers for ' time(1,1:10)])
